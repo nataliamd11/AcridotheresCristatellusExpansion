a325bb9deb33137ddaa6ef233d3e7aa0732519a2
nataliamd11 <nataliamdenk11@gmail.com>
2021-05-25 09:49:40 -0300

    AC Records model

--- AcridotheresCristatellusExpansion/ACExpansion/models.py
+++ AcridotheresCristatellusExpansion/ACExpansion/models.py
@@ -1,3 +1,31 @@
+import uuid
+from datetime import date
+
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+def get_year(instance):
+    return instance.observation_date.year
+
+
+class ACRecord(models.Model):
+    
+    id = models.UUIDField(primary_key=True,
+                          default=uuid.uuid4,
+                          editable=False)
+    observation_count = models.IntegerField(null=True)
+    country = models.CharField(max_length=150)
+    country_code = models.CharField(max_length=2)
+    state = models.CharField(max_length=150)
+    state_code = models.CharField(max_length=150)
+    latitude = models.FloatField(validators=[MinValueValidator(-90), 
+                                             MaxValueValidator(90)])
+    longitude = models.FloatField(validators=[MinValueValidator(-180), 
+                                              MaxValueValidator(180)])
+    observation_date = models.DateField(null=False)
+    year = models.IntegerField(default=get_year)
+
+
+    def __str__(self):
+        return f'{self.country_code} _ {self.year}'
