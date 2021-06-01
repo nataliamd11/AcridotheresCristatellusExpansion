6e72779762f6025a2a5c5844b3f3d5586ec30d32
nataliamd11 <nataliamdenk11@gmail.com>
2021-06-01 12:36:45 -0300

    Play & Stop getting records data

--- AcridotheresCristatellusExpansion/frontend/src/store/index.js
+++ AcridotheresCristatellusExpansion/frontend/src/store/index.js
@@ -4,17 +4,17 @@ import axios from "axios";
 export default createStore({
   state: {
     records: [],
-    years: []
+    years: [],
   },
   getters: {
     getRecordsByYear: (state) => (year) => {
-      return state.records.filter(records_year => records_year.year === year)
+      return state.records.filter((records_year) => records_year.year === year);
     },
-    Countries:state => {
-      return state.countries
-    }, 
-    getYears:state => {
-      return state.years
+    Countries: (state) => {
+      return state.countries;
+    },
+    getYears: (state) => {
+      return state.years;
     },
   },
   mutations: {
@@ -23,21 +23,20 @@ export default createStore({
     },
     saveYears(state, payload) {
       state.years = payload.years;
-    }
+    },
   },
   actions: {
     getAPIRecords(context, siteParams) {
-      let endpoint = ['api', 'records', siteParams.country, ''].join('/');
-      axios.all([
-        axios.get(endpoint),
-        axios.get(endpoint + 'years/')
-      ])
-      .then(axios.spread((records, years) => {
-          context.commit('saveRecords', { 'records': records.data}),
-          context.commit('saveYears', { 'years': years.data})
-        }
-      ))
-      .catch(err => console.log(err));
+      let endpoint = ["api", "records", siteParams.country, ""].join("/");
+      axios
+        .all([axios.get(endpoint), axios.get(endpoint + "years/")])
+        .then(
+          axios.spread((records, years) => {
+            context.commit("saveRecords", { records: records.data }),
+            context.commit("saveYears", { years: years.data });
+          })
+        )
+        .catch((err) => console.log(err));
     },
   },
   modules: {},
--- AcridotheresCristatellusExpansion/frontend/src/views/Home.vue
+++ AcridotheresCristatellusExpansion/frontend/src/views/Home.vue
@@ -1,61 +1,70 @@
 <template>
   <div class="container">
     <select v-model="selectedCountry">
-        <option>Argentina</option>
-        <option>Canada</option>
-        <option>Uruguay</option>
+      <option>Argentina</option>
+      <option>Canada</option>
+      <option>Uruguay</option>
     </select>
     <span>{{ selectedCountry }}</span>
   </div>
-  <div v-for="(year, index) in ACYears" :key="index">
+  <!-- <div v-for="(year, index) in ACYears" :key="index">
     {{ year }}
+  </div> -->
+  <div class="mt-3">
+    <button v-if="playMap" @click="playAnimatedMap">Play</button>
+    <button v-else @click="stopAnimatedMap">Stop</button>
   </div>
   <div v-for="record in ACRecords" :key="record.id">
     {{ record }}
   </div>
-  <div class='mt-3'>
-    <button @click="test">Play</button>
-  </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions } from "vuex";
+import  { maxMinYears, sleep } from "/common/utils.js";
 
 export default {
   name: "Home",
-  data: function() {
-      return {
-          selectedCountry: 'Argentina',
-          year: 1988,
-      }
+  data: function () {
+    return {
+      selectedCountry: "Argentina",
+      year: null,
+      playMap: true,
+    };
   },
   watch: {
     selectedCountry() {
-        this.showCountry(this.selectedCountry);
-        this.getAPIRecords({country: this.selectedCountry});
+      this.year = null;
+      this.playMap = true;
+      this.getAPIRecords({ country: this.selectedCountry });
+    },
+  },
+  async mounted() {
+    await this.getAPIRecords({ country: this.selectedCountry });
+  },
+  computed: {
+    ACRecords() {
+      return this.$store.getters.getRecordsByYear(this.year);
+    },
+    ACYears() {
+      return this.$store.getters.getYears;
+    },
+  },
+  methods: {
+    ...mapActions(["getAPIRecords"]),
+    async playAnimatedMap() {
+      let [minYear, maxYear] = maxMinYears(this.ACYears);
+      this.playMap = false;
+      let counter = minYear;
+      while (counter <= maxYear && this.playMap === false) {
+       this.year = counter;
+       await sleep(500);
+       counter++;
+      }
+    },
+    stopAnimatedMap() {
+      this.playMap = true;
     }
   },
-  // async mounted() {
-  //       await this.getAPIRecords({country: this.selectedCountry});
-  //   },
-  computed:{
-      ACRecords() {
-        return this.$store.getters.getRecordsByYear(this.year)
-      },
-      ACYears() {
-        return this.$store.getters.getYears
-      },
-    },
-  methods: {
-        ...mapActions([
-        'getAPIRecords'
-        ]),
-        showCountry(country_in) {
-            console.log(country_in)
-        },
-        test() {
-          console.log('clicked');
-        }
-    },
 };
 </script>
