6bb9e2649ab7fa5ca94d0f8d12fb1fe1dc589574
nataliamd11 <nataliamdenk11@gmail.com>
2021-06-02 15:21:01 -0300

    lint -- fix

--- AcridotheresCristatellusExpansion/frontend/src/components/Map.vue
+++ AcridotheresCristatellusExpansion/frontend/src/components/Map.vue
@@ -1,38 +1,36 @@
 <template>
- <div class="container">
-   <h1>Showing component</h1>
-   <div id="mapContainer">
-
-   </div>
- </div>
+  <div class="container">
+    <h1>Showing component</h1>
+    <div id="mapContainer"></div>
+  </div>
 </template>
-
 
 <script>
 import { createMap, addTile, createMarker } from "/utils/utils.js";
 
 export default {
- name: "Map",
- data() {
-   return{
-     center: {lat: 51.505, lon: -0.09}
-   }},
- methods: {
-   setupLeafletMap: function () {
-     var map = createMap('mapContainer', this.center);
-     addTile(map);
-     createMarker(this.center, map);
-   }
- },
- mounted() {
-   this.setupLeafletMap()
- }
+  name: "Map",
+  data() {
+    return {
+      center: { lat: 51.505, lon: -0.09 },
+    };
+  },
+  methods: {
+    setupLeafletMap: function () {
+      var map = createMap("mapContainer", this.center);
+      addTile(map);
+      createMarker(this.center, map);
+    },
+  },
+  mounted() {
+    this.setupLeafletMap();
+  },
 };
 </script>
 
 <style scoped>
 #mapContainer {
- width: 500px;
- height: 500px;
+  width: 500px;
+  height: 500px;
 }
 </style>
