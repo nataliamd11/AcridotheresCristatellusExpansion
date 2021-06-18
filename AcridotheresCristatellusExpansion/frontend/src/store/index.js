e95929d95524353a5edfe22cbd9ca46e8217c2d0
nataliamd11 <nataliamdenk11@gmail.com>
2021-06-18 18:17:23 -0300

    API country list in store index.js

--- AcridotheresCristatellusExpansion/frontend/src/store/index.js
+++ AcridotheresCristatellusExpansion/frontend/src/store/index.js
@@ -1,9 +1,49 @@
 import { createStore } from "vuex";
-import ApiData from './modules/ApiData'
+import axios from "axios";
+import ApiData from './modules/ApiData';
+import MapData from './modules/MapData';
 
 
 export default createStore({
+  
   modules: {
       ApiData,
+      MapData,
   },
+
+  state: {
+    countries: [],
+  },
+
+  getters: {
+    getCountries: (state) => {
+      return state.countries;
+    },
+  },
+
+  mutations: {
+    saveCountries(state, payload) {
+      state.countries = payload.countries;
+    },
+  },
+
+  actions: {
+
+    async getAPICountryList(context) {
+      console.log('entra: getcountries action');
+      // Gets data from django api and saves it in state.
+      let endpoint = ["api", "records", "countries", ""].join("/");
+      try {
+        let countries = await axios.get(endpoint);
+        console.log('countries data: ', countries.data);
+        context.commit("saveCountries", { countries: countries.data });
+      } catch(err) {
+        if (err.response) {
+          console.log('error: ', err.response.status);
+        }
+      }
+    },
+
+  }
+
 });
